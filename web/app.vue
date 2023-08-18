12257c39c00befc5f3b9a40f2d3a71c36c237e74
Ayu Nakata <mieze018@users.noreply.github.com>
2023-08-18 17:44:57 +0900

    wip テストデータをVueで表示

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const { data: healthChecks } = await useFetch(
+  'http://localhost:3004/api/v1/healthchecks'
+)
+const title = 'Rails API Nuxt TODO App'
+console.log(healthChecks)
+</script>
 <template>
   <div>
+    <h2>Heath Check</h2>
+    <p>Count: {{ healthChecks }}</p>
+
     <NuxtWelcome />
   </div>
 </template>
