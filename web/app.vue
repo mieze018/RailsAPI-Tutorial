2efb0879a75037ba6b9899ec4e5d44b2c335d368
Ayu Nakata <mieze018@users.noreply.github.com>
2023-08-25 18:25:51 +0900

    APIの値をフロントエンドで取得できる

--- web/app.vue
+++ web/app.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-// 'http://localhost:3004/api/v1/healthchecks' からuseFetchを使ってデータを取得
-const { data: healthChecks } = useFetch(
-  'http://localhost:3004/api/v1/healthchecks'
-)
+const healthChecks = ref()
+const loading = ref(true)
 
+onMounted(async () => {
+  const { pending, data } = await useFetch(
+    'http://localhost:3004/api/v1/healthchecks'
+  )
+  loading.value = pending.value
+  healthChecks.value = data.value.request
+})
 const title = 'Rails API Nuxt TODO App'
-console.log(healthChecks)
 </script>
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>Count: {{ healthChecks }}</p>
+    <div v-if="loading">Loading ...</div>
+    <p v-if="!loading">healthChecks: {{ healthChecks }}</p>
 
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
