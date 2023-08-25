4ae861b68c6d04c6924620cadae6a58a4548fbae
Ayu Nakata <mieze018@users.noreply.github.com>
2023-08-25 15:23:14 +0900

    Update app.vue

--- web/app.vue
+++ web/app.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-const { data: healthChecks } = await useFetch(
+// 'http://localhost:3004/api/v1/healthchecks' からuseFetchを使ってデータを取得
+const { data: healthChecks } = useFetch(
   'http://localhost:3004/api/v1/healthchecks'
 )
+
 const title = 'Rails API Nuxt TODO App'
 console.log(healthChecks)
 </script>
 <template>
   <div>
-    <h2>Heath Check</h2>
+    <h1>{{ title }}</h1>
     <p>Count: {{ healthChecks }}</p>
 
     <NuxtWelcome />
