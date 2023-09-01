c0e453121b3624f452d4a7363d706dd89dbdb22d
Ayu Nakata <mieze018@users.noreply.github.com>
2023-09-01 17:53:14 +0900

    ルーティング追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+  root "index#index"
   namespace :api do
     namespace :v1 do
       resources :healthchecks, only: %i(index)
+      resources :todos, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
