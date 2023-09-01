4264e5ceddefb2d0e197bd5b5f7d46cdbe9ab6af
Ayu Nakata <mieze018@users.noreply.github.com>
2023-09-01 17:12:13 +0900

    エラーキャッチを追加

--- web/app.vue
+++ web/app.vue
@@ -3,11 +3,15 @@
   const loading = ref(true);
 
   onMounted(async () => {
-    const { pending, data } = await useFetch<{ request: string }>(
-      'http://localhost:3004/api/v1/healthchecks',
-    );
-    loading.value = pending.value;
-    healthChecks.value = data.value?.request;
+    try {
+      const { pending, data } = await useFetch<{ request: string }>(
+        'http://localhost:3004/api/v1/healthchecks',
+      );
+      loading.value = pending.value;
+      healthChecks.value = data.value?.request;
+    } catch (err) {
+      console.error(err);
+    }
   });
   const title = 'Rails API Nuxt TODO App';
 </script>
@@ -16,7 +20,7 @@
     <h1>{{ title }}</h1>
     <div v-if="loading">Loading ...</div>
     <p v-if="!loading">healthChecks: {{ healthChecks }}</p>
-
-    <!-- <NuxtWelcome /> -->
+    <!-- エラーだった場合 -->
+    <p v-if="!loading && !healthChecks">エラーが発生しました</p>
   </div>
 </template>
