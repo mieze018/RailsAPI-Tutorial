e7fd7ff3d5ba5e659402132c552878dc9ca3b882
Ayu Nakata <mieze018@users.noreply.github.com>
2023-09-15 21:12:06 +0900

    リストにして表示

--- web/components/Todos/Index.vue
+++ web/components/Todos/Index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-  const dataRef = ref<string | null | undefined>(null);
+  const dataRef = ref();
   const loading = ref(true);
 
   onMounted(async () => {
     try {
-      const { pending, data } = await useFetch<{ request: string }>(
+      const { pending, data } = await useFetch(
         'http://localhost:3004/api/v1/todos',
       );
       loading.value = pending.value;
@@ -17,7 +17,12 @@
 <template>
   <div>
     <div v-if="loading">Loading ...</div>
-    <p v-if="!loading">Todos: {{ dataRef }}</p>
+    <p v-if="!loading">Todos</p>
+    <ul>
+      <li v-for="todo in dataRef" :key="todo.id">
+        {{ todo.content }}
+      </li>
+    </ul>
     <!-- エラーだった場合 -->
     <p v-if="!loading && !dataRef">エラーが発生しました</p>
   </div>
